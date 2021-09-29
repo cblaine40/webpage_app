5f605d6a0eb5c77b7c41ee88bc009e24bfda7a78
cblaine40 <cblaine@vt.edu>
2021-09-29 21:27:35 +0000

    Need authentication still

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_174005) do
+ActiveRecord::Schema.define(version: 2021_09_29_211522) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "create_at"
     t.datetime "update_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
